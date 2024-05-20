15a3399e666bcc1a37ed1951174803e2d52ba08b
rifahad <rifahadmt202@gmail.com>
2024-05-20 22:42:45 +0530

    Started with header design

---
+++ RigDrocks/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Header from './Components/User/Header'
+
+function App() {
+  
+
+  return (
+    <>
+      <Header/>
+    </>
+  )
+}
+
+export default App
---
+++ RigDrocks/src/Components/User/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='w-lvw h-[100px] bg-black flex'>
+        <div className='w-[50%] bg-blue-100 h-full'>
+
+        <h1 className='sm:text-2xl md:text-4xl'>REDROCKS</h1>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Header
